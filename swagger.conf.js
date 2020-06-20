589bb0eba8cf60c557f074d1be18b8be0ad9be86
P4elkaB35t <saganiter234@gmail.com>
2020-06-20 16:34:07 +0300

    Swagger API update

---
+++ swagger.conf.js
@@ -0,0 +1,8 @@
+module.exports = {
+    info: {
+        // API informations (required)
+        title: 'TO-DO LIST API', // Title (required)
+        version: '3.0.0', // Version (required)
+        description: 'API for auth and to-do list interactions', // Description (optional)
+    }
+};
